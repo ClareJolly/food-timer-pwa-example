1b772a2386c9b544b9e8dc5d3aa1230597daf7ba
Clare Jolly <clarejollyuk@gmail.com>
2019-05-08 23:59:46 +0100

    Updates

---
+++ precache-manifest.75067f2c0417fb5f1a68e01ee5906d05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0947e6fa8c4a369fc2e7f9aa25e2f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fdb4ac3651ee1fb2408",
+    "url": "/static/css/main.51d40ce3.chunk.css"
+  },
+  {
+    "revision": "39529e546a82aba6b124",
+    "url": "/static/js/2.14d346ba.chunk.js"
+  },
+  {
+    "revision": "1fdb4ac3651ee1fb2408",
+    "url": "/static/js/main.2b7aab5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
